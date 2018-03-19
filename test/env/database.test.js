aa00564d543f6dc2957b6544386a93f51ce0883c
Sebastian Van Sande <sebastian@vansande.org>
2018-03-19 12:00:22 +0100

    Improve test coverage to 100%

--- test/env/database.test.js
+++ test/env/database.test.js
@@ -6,10 +6,12 @@ const proxyquire = require('proxyquire');
 
 describe('database', function () {
 
+  let configObj
   let database; // module under test
   let configFile, mongodb, client; // mocked dependencies
 
   beforeEach(function () {
+    configObj = createConfigObj();
     client = mockClient();
     configFile = mockConfigFile();
     mongodb = mockMongodb();
@@ -38,7 +40,45 @@ describe('database', function () {
         done();
       });
     });
+
+    it('should yield an error when no url is defined in the config file', (done) => {
+      delete configObj.mongodb.url;
+      database.connect((err) => {
+        expect(err.message).to.equal('No `url` defined in config file!');
+        return done();
+      });
+    });
+
+    it('should yield an error when no databaseName is defined in the config file', (done) => {
+      delete configObj.mongodb.databaseName;
+      database.connect((err) => {
+        expect(err.message).to.equal('No `databaseName` defined in config file! This is required since migrate-mongo v3. ' +
+          'See https://github.com/seppevs/migrate-mongo#initialize-a-new-project');
+        return done();
+      });
+    });
+
+    it('should yield an error when unable to connect', (done) => {
+      mongodb.MongoClient.connect.yields(new Error('Unable to connect'));
+      database.connect((err) => {
+        expect(err.message).to.equal('Unable to connect');
+        return done();
+      });
+    });
   });
+
+  function createConfigObj() {
+    return {
+      mongodb: {
+        url: 'mongodb://someserver:27017',
+        databaseName: 'testDb',
+        options: {
+          connectTimeoutMS: 3600000, // 1 hour
+          socketTimeoutMS: 3600000, // 1 hour
+        }
+      }
+    };
+  }
 
   function mockClient() {
     return {
@@ -48,16 +88,7 @@ describe('database', function () {
 
   function mockConfigFile() {
     return {
-      read: sinon.stub().returns({
-        mongodb: {
-          url: 'mongodb://someserver:27017',
-          databaseName: 'testDb',
-          options: {
-            connectTimeoutMS: 3600000, // 1 hour
-            socketTimeoutMS: 3600000, // 1 hour
-          }
-        }
-      })
+      read: sinon.stub().returns(configObj)
     };
   }
 
