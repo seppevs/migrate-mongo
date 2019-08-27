3d0fdd71667600fcde7d39ea6a616390e78b7284
Sebastian Van Sande <sebastian@vansande.org>
2019-08-27 09:51:00 +0200

    Bump dependencies

--- samples/migrate-mongo-config.js
+++ samples/migrate-mongo-config.js
@@ -9,7 +9,8 @@ const config = {
     databaseName: "YOURDATABASENAME",
 
     options: {
-      useNewUrlParser: true // removes a deprecation warning when connecting
+      useNewUrlParser: true, // removes a deprecation warning when connecting
+      useUnifiedTopology: true, // removes a deprecating warning when connecting
       //   connectTimeoutMS: 3600000, // increase connection timeout to 1 hour
       //   socketTimeoutMS: 3600000, // increase socket timeout to 1 hour
     }
