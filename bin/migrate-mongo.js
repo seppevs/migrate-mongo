43a9e888661bfbd9d477979a2e2bf6a921152d2b
Sukhpal Saini <bdcorps@users.noreply.github.com>
2020-10-02 02:04:50 -0400

    fix description for up command

--- bin/migrate-mongo.js
+++ bin/migrate-mongo.js
@@ -58,8 +58,8 @@ program
 program
   .command("up")
   .description("run all pending database migrations")
-  .option("-c --custom <custom>", "use a custom config file")
-  .option("-f --file <file>", "use a file")
+  .option("-c --custom <custom>", "run a custom migration file")
+  .option("-f --file <file>", "use a custom config file")
   .action(options => {
     global.options = options;
     migrateMongo.database
