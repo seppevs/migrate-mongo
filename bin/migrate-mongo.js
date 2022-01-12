bbff8781a0e5c62724aa03dd2bd93de09bed78a5
Eduardo Backes <e.backes2011@gmail.com>
2022-01-12 17:15:44 -0300

    fix: exit from process after create command is done

--- bin/migrate-mongo.js
+++ bin/migrate-mongo.js
@@ -56,6 +56,9 @@ program
           console.log(`Created: ${config.migrationsDir}/${fileName}`);
         })
       )
+      .then(() => {
+        process.exit(0);
+      })
       .catch(err => handleError(err));
   });
 
